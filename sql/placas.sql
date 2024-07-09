93c6fe67cb35f204ce952f3e1342f2846173b628
Hugo Ariel Castroman <hcastroman@gmail.com>
2024-07-09 03:05:23 -0300

    Se alterna orden de creacion de tablas

--- sql/placas.sql
+++ sql/placas.sql
@@ -181,6 +181,7 @@ INSERT INTO `compra_productos` (`compra_id`, `producto_id`, `cantidad`, `subtota
 	(9, 15, 2, 10000),
 	(10, 11, 1, 7999);
 
+
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
