bddc3d40c6b91ae43740f0a07911e6b3575d94d8
Hugo Ariel Castroman <hcastroman@gmail.com>
2024-07-09 02:31:55 -0300

    Add Proyecto Backend

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('../config/config');
+const db = require('../db/db');
+
+exports.register = (req,res) => {
+    const {nombre, email, telefono, password, cuil} = req.body;
+    const hashedPassword  = bcrypt.hashSync(password,8);
+    const sql = 'INSERT INTO clientes (nombre, email, telefono, password, cuil) VALUES (? , ? , ? , ? , ?)';
+    const values = [nombre, email, telefono, hashedPassword, cuil];
+    db.query(sql, values, (err,result)=>{
+        if (err) {
+            console.log(err);
+            return res.status(500).json({error: "Intente mas tarde"});
+        }
+        const token = jwt.sign({user: email},config.secretKey,{expiresIn:config.tokenExpiresIn});
+        res.status(201).send({auth:true,token});
+    });
+};
+    
+exports.login = (req,res) => {
+    const {email, password} = req.body;
+    const sql = 'SELECT password FROM clientes where email = ? ';
+    db.query(sql, [email], (err,result)=>{
+        if (err) {
+            console.log(err);
+            return res.status(500).json({error: "Intente mas tarde"});
+        }
+        if(result=='') return res.status(404).send(`El email ${email} no se encuentra registrado`);
+        const passwordIsValid = bcrypt.compareSync(password,result[0].password);
+        if (!passwordIsValid) return res.status(401).send({auth:false, token:null});
+        const token = jwt.sign({user: email},config.secretKey,{expiresIn: config.tokenExpiresIn});
+        res.status(200).send({auth:true,token});
+    });
+};
+
+exports.cliente = (req,res) => {
+    const user =  req.user;
+    const sql = 'SELECT nombre, email, telefono, cuil FROM clientes where email = ? ';
+    db.query(sql,[user],(err,result)=>{
+        if (err) {
+            console.log(err);
+            return res.status(500).json({error: "Intente mas tarde"});
+        }
+        res.json(result);
+    });
+};
